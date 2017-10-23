f9a69de0da68e4e58be175842ab19a451812f3d6
bryan buitrago <bryanbuitrago86@gmail.com>
2017-10-23 15:18:04 -0400

    Part 2 completed

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,21 +1,30 @@
 <template lang="html">
-  <div>
-    <h1>Register</h1>
-
-    <input
-      type="email"
-      name="email"
-      v-model="email"
-      placeholder="email">
-    <br>
-    <input
-      type="password"
-      name="password"
-      v-model="password"
-      placeholder="password">
-    <br>
-    <button type="button" name="button" @click="register">Register</button>
-  </div>
+  <v-layout>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+      <div class="pl-4 pr-4 pt-2 pb-2">
+        <input
+          type="email"
+          name="email"
+          v-model="email"
+          placeholder="email">
+        <br>
+        <input
+          type="password"
+          name="password"
+          v-model="password"
+          placeholder="password">
+        <br>
+        <div class="error" v-html="error" />
+        <br>
+        <v-btn class="cyan" @click="register">Register</v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -24,7 +33,8 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   // watch: {
@@ -34,15 +44,22 @@ export default {
   // },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      // console.log(response.data)
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
 </script>
 
 <style scoped lang="css">
+  .error {
+    color: white;
+  }
 </style>
